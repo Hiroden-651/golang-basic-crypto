67a7ad42f5f98c01c0ff15784f79400073a8a9a0
Bradley Maness <52007264+Hiroden-651@users.noreply.github.com>
2020-06-07 21:11:55 -0700

    ignore non-letters for encryption/decryption.

--- vigenere/vigenere.go
+++ vigenere/vigenere.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+// Create a byte slice encryption/decryption key from a string argument.
+func createKey(s string) []byte {
+	myKey := []byte(s)
+	for i := range myKey {
+		myKey[i] -= 64
+	}
+	return myKey
+}
+
+// Return true if a byte value is an upper case letter(ASCII).
+func isLetter(a byte) bool {
+	if a >= 65 && a <= 95 {
+		return true
+	}
+	return false
+}
+
+// Modify original byte value 'a' using a key byte value 'b'. Return 'a'.
 func encryptByte(a, b byte) byte {
 	a += b
 	if a > 90 {
@@ -10,18 +28,22 @@ func encryptByte(a, b byte) byte {
 	return a
 }
 
+// Encrypt cleartext byte slice using key byte slice. Return the encrypted message.
 func encrypt(cleartext, key []byte) []byte {
 	keyIndex := 0
 	for i := range cleartext {
-		cleartext[i] = encryptByte(cleartext[i], key[keyIndex])
-		keyIndex++
-		if keyIndex >= len(key) {
-			keyIndex = 0
+		if isLetter(cleartext[i]) {
+			cleartext[i] = encryptByte(cleartext[i], key[keyIndex])
+			keyIndex++
+			if keyIndex >= len(key) {
+				keyIndex = 0
+			}
 		}
 	}
 	return cleartext
 }
 
+// Modify byte 'a' by value of byte 'b'. Return 'a'.
 func decryptByte(a, b byte) byte {
 	a -= b
 	if a < 65 {
@@ -30,13 +52,16 @@ func decryptByte(a, b byte) byte {
 	return a
 }
 
+// Decrypt ciphertext using the same key used for encrypting the plaintext. Return the deciphered text.
 func decrypt(ciphertext, key []byte) []byte {
 	keyIndex := 0
 	for i := range ciphertext {
-		ciphertext[i] = decryptByte(ciphertext[i], key[keyIndex])
-		keyIndex++
-		if keyIndex >= len(key) {
-			keyIndex = 0
+		if isLetter(ciphertext[i]) {
+			ciphertext[i] = decryptByte(ciphertext[i], key[keyIndex])
+			keyIndex++
+			if keyIndex >= len(key) {
+				keyIndex = 0
+			}
 		}
 	}
 	return ciphertext
@@ -44,20 +69,19 @@ func decrypt(ciphertext, key []byte) []byte {
 
 func main() {
 
-	myKey := []byte("HELLO")
-	fmt.Println(string(myKey))
+	keyString := "TESTKEY"
+	fmt.Printf("Cipher key:\t\t%s\n", keyString)
 
-	for i := range myKey {
-		myKey[i] -= 64
-	}
+	myKey := createKey(keyString)
 
 	// create a cleartext message:
-	cleartext := []byte("THISISMYMESSAGE")
+	cleartext := []byte("THIS IS MY MESSAGE.")
+	fmt.Printf("Cleartext message:\t%s\n", string(cleartext))
 
 	ciphertext := encrypt(cleartext, myKey)
-	fmt.Println(string(ciphertext))
+	fmt.Printf("Ciphertext message:\t%s\n", string(ciphertext))
 
 	decrypted := decrypt(ciphertext, myKey)
-	fmt.Println(string(decrypted))
+	fmt.Printf("Decrypted message:\t%s\n", string(decrypted))
 
 }
